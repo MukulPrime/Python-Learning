bed653a6a16de60132d86f9611b6d1d9f2b33191
Mukul Rana <your-github-ar7167145@gmail.com>
2026-07-01 23:11:51 +0530

    Organized variables practice

---
+++ practice python/q3.py
@@ -0,0 +1,10 @@
+# Calculate area of rectangle
+def area_ofrectangle():
+    a = int(input("Enter the value of Length: "))
+    b = int(input("Enter the value of Width: "))
+
+    area = a * b
+
+    print(f"Area of rectangle is: {area}")
+
+area_ofrectangle()  
