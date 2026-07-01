90015b2b3e76134a68305bada3fd5e88c0a625aa
MukulPrime <ar7167145@gmail.com>
2026-07-02 00:23:22 +0530

    Delete practice python directory

--- practice python/q1.py
+++
@@ -1,8 +0,0 @@
-#ask user for it's name and age 
-def user_info():
-    name = input("What is your name? ")
-    age = int(input("How old are you ?"))
-    print(f"Hello, {name} you are {age} years old.")
-    print("GOOD BOY!")
-
-user_info()
