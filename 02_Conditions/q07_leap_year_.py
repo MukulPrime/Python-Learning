525ff08df7ff778033bd5e8f30665914059f7051
Mukul Rana <your-github-ar7167145@gmail.com>
2026-07-04 21:57:44 +0530

    Leap year program

---
+++ 02_Conditions/q07_leap_year_.py
@@ -0,0 +1,10 @@
+# Leap year checker program
+def leap_year():
+    year = int(input("Enter the year: "))
+
+    if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+        print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is not a leap year.")
+
+leap_year()
