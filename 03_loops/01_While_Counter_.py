e03636174f3aea53cd52f32a7ea2212b2dfbadd8
Mukul Rana <your-github-ar7167145@gmail.com>
2026-07-10 23:36:14 +0530

    while loop counter

---
+++ 03_loops/01_While_Counter_.py
@@ -0,0 +1,11 @@
+# create a program to print 1 to 5 using while loop 
+def main():
+
+    count = 1
+    while count <= 5:   
+        print(count)
+        count += 1
+        
+    print("Loop Finished")
+
+main()
